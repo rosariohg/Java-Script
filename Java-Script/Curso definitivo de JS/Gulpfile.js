f2cde204b9af6463fb71acec558cfde55ff1f3f2
rosariohg <rhuancagonza@gmail.com>
2017-07-10 19:55:39 -0700

    watchify escucha y recompila los cambios, scripts nos permitira organizar el proyecto

--- Java-Script/Curso definitivo de JS/Gulpfile.js
+++ Java-Script/Curso definitivo de JS/Gulpfile.js
@@ -5,6 +5,9 @@ var babel = require('babelify');
 var browserify = require('browserify');
 var presets =  require('babel-preset-es2015');
 var source = require('vinyl-source-stream');
+var watchify = require('watchify');
+
+
 
 gulp.task('styles',function(){
 	gulp
@@ -21,15 +24,37 @@ gulp.task('assets',function(){
 		.pipe(gulp.dest('public'));
 })
 
+function compile(watch){
 
-gulp.task('scripts', function () {
-  browserify('./src/index.js')
-    .transform(babel, {presets: ["es2015"]})
-    .bundle()
-    .pipe(source('index.js'))
-    .pipe(rename('app.js'))
-    .pipe(gulp.dest('public'));
+	var bundle = watchify(browserify('./src/index.js'));
+	function rebundle(){
+		//nos referimos a variables def fuera
+		
+		bundle
+			.transform(babel, {presets: ["es2015"]})
+		    .bundle()
+		    .pipe(source('index.js'))
+		    .pipe(rename('app.js'))
+		    .pipe(gulp.dest('public'));
+	}
+
+	if (watch){
+		bundle.on('update',function(){
+			console.log('Bundling ...');
+			rebundle();
+		})
+	}
+
+	rebundle()
+}
+
+gulp.task('build',function(){
+	return compile();
+});
+
+gulp.task('watch',function(){
+	return compile(true);
 });
 
 
-gulp.task('default',['styles','assets','scripts']);
+gulp.task('default',['styles','assets','build']);
--- Java-Script/Curso definitivo de JS/src/index.js
+++ Java-Script/Curso definitivo de JS/src/index.js
@@ -1,5 +1,13 @@
-var numeros = [ 400, 200, 1, -23 ];
+var page = require('page');
 
-var numerosMas1 = numeros.map(n => n + 1)
+var main = document.getElementById('main-container')
 
-console.log(numerosMas1);
+page('/',function(ctx,next){
+	main,innerHTML = 'Home'; //home
+})
+
+page('/signup',function(ctx,next){
+	main,innerHTML = 'Signup'; // signup
+})
+
+page.start();
